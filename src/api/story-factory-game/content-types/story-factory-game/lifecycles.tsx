613896332ecc758f04bc80239c846919bfc89e57
Aleksandra Makhrova <amakhro@gmail.com>
2023-12-05 21:30:41 -0800

    Created firebase.ts with credentials and methods were added to story-factory lifecycle.tsx to set, update and delete data

--- src/api/story-factory-game/content-types/story-factory-game/lifecycles.tsx
+++ src/api/story-factory-game/content-types/story-factory-game/lifecycles.tsx
@@ -1,7 +1,35 @@
-import {uuid} from 'uuidv4';
+import { uuid } from 'uuidv4';
+import { db } from "../../../../../config/firebase"
+import { doc, setDoc, deleteDoc, updateDoc } from 'firebase/firestore';
 
-module.exports = {
-    async beforeCreate(event){
-	event.params.data.uuid = uuid();
+const COLLECTION = "story-factory-game"
+
+export default {
+    beforeCreate(event) {
+        event.params.data.uuid = uuid();
+    },
+
+    async afterCreate(event) {
+        const { result } = event;
+
+        await setDoc(doc(db, COLLECTION, result.uuid), {
+            handle: result.handle,
+            pack_name: result.pack_name,
+            word_group: result.word_group,
+        });
+    },
+
+    async afterUpdate(event) {
+        const { result } = event;
+        await updateDoc(doc(db, COLLECTION, event.result.uuid), {
+            handle: result.handle,
+            pack_name: result.pack_name,
+            word_group: result.word_group,
+        })
+        
+    },
+
+    async afterDelete(event) {
+        await deleteDoc(doc(db, COLLECTION, event.result.uuid))
     }
-};
+}
